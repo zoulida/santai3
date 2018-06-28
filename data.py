f0b85c8e36965d4dacdc3e28b3d6408badbca9ad
zoulida <zoulida@163.com>
2018-06-28 22:49:03 +0800

    Initial commit

---
+++ data.py
@@ -0,0 +1,14 @@
+import tushare as ts
+
+# print(ts.get_hist_data('600848'))
+# print(ts.cap_tops())
+
+# print(ts.inst_tops())
+
+# print(ts.broker_tops())
+# print(ts.get_hist_data('600848', ktype='5'))
+
+
+
+df = ts.get_today_ticks('600018')
+print(df)
