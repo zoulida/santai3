7ae87621f43d22c878936472d30819918193531e
zoulida <zoulida@163.com>
2018-07-06 16:35:44 +0800

    aa

--- testMysql.py
+++ testMysql.py
@@ -2,13 +2,19 @@ __author__ = 'Administrator'
 
 from sqlalchemy import create_engine
 
-engine = create_engine('mysql://root:root@127.0.0.1/stockdata0?charset=utf8')
+#engine = create_engine('mysql://root:root@127.0.0.1/stockdata0?charset=utf8')
 
-#存入数据库
-#df.to_sql('tick_data',engine)
+import pandas as pd
+from pandas import Series, DataFrame
+
+data = {"name":["yahoo","google","facebook"], "marks":[200,400,800], "price":[9, 3, 7]}
+f1 = DataFrame(data)
+
+print(f1)
+f1["price"]=[ 'dd % i' % i for i in f1["price"]]
+print(f1)
+print(f1["price"])
 
 
 
-df =[fda,da,fdsa]
-#追加数据到现有表
-df.to_sql('tick_data',engine,if_exists='append')
+#df.to_sql('tick_data',engine,if_exists='append')
