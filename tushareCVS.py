7ad0abe8c4169136ba8510c8b7fa9f62e4de6d86
zoulida <zoulida@163.com>
2018-07-10 06:54:33 +0800

    yyy

--- tushareCVS.py
+++ tushareCVS.py
@@ -3,13 +3,13 @@ import mkdir
 
 mkdir.mkdirA("d:/day")
 
-df = ts.get_hist_data('000875')
+df = ts.get_hist_data('300022')
 #直接保存
-df.to_csv('d:/day/000875.csv')
+df.to_csv('d:/day/300022.csv')
 
 #print(ts.get_k_data('600000'))
 
-print(ts.get_k_data('600000', ktype='5'))
+print(ts.get_k_data('300022', ktype='5'))
 
 #选择保存
 #df.to_csv('d:/day/000875.csv',columns=['open','high','low','close'])
