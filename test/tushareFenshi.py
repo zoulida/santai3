ebb31a215613a55a04b379ceb3e803f0fbf11093
Administrator <zoulida@163.com>
2019-05-13 15:55:10 +0800

    office1701

---
+++ test/tushareFenshi.py
@@ -0,0 +1,10 @@
+__author__ = 'zoulida'
+import tushare as ts
+
+df1 = ts.get_realtime_quotes('000581') #Single stock symbol
+#df[['code','name','price','bid','ask','volume','amount','time']]
+#print(df1)
+
+df = ts.get_today_ticks()
+
+print(df)
