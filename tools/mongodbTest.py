764972a219de5bf7db66050fcc95e7f9911e8afb
Administrator <zoulida@163.com>
2019-07-23 07:01:27 +0800

    office1701

--- tools/mongodbTest.py
+++ tools/mongodbTest.py
@@ -10,10 +10,15 @@ from pymongo import MongoClient
 from tools.mongodbFactory import getConnectionWuDuJi
 wenduji = getConnectionWuDuJi()
 
+#wenduji.remove()#全部删除
+
+#pattern = re.compile("del") #模糊查询预编译
+#collection.remove({"name":pattern})
+#collection.remove()全部删除
 
 my_set = wenduji #使用test_set集合，没有则自动创建
 
-my_set.insert({"name":"zhangsan","age":118})
+#my_set.insert({"name":"zhangsan","age":118})
 #或
 #my_set.save({"name":"zhangsan","age":18})
 
@@ -27,8 +32,11 @@ users=[{"name":"zhangsan","age":18},{"name":"lisi","age":20}]
 # for i in my_set.find():
 #     print(i)
 #查询name=zhangsan的
-for i in my_set.find({"name":"zhangsan"}):
-    print(i)
+#for i in my_set.find({"name":"zhangsan"}):
+#    print(i)
+print(my_set.count())
 for i in my_set.find():
     print(i)
 #print(my_set.find_one({"name":"zhangsan"}))
+
+#wenduji.remove()#全部删除
