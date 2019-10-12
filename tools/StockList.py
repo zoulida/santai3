8911972e0535053aec14e782ecf5bfc4141cecc1
Administrator <zoulida@163.com>
2019-10-12 11:45:13 +0800

    office1701

--- tools/StockList.py
+++ tools/StockList.py
@@ -3,7 +3,11 @@ __author__ = 'zoulida'
 import tushare as ts
 import pandas as pd
 from tools.LogTools import Logger #注意可以.LogTools import Logger相对路径更为简洁一些
+from memory_profiler import profile
+
 logger = Logger(logName='log.txt', logLevel="DEBUG", logger="logTest.py").getlog()
+
+@profile
 def get_all_stock2():
     import tools.platformPrint as pp
     import tools.mkdir as mkdir
@@ -19,20 +23,42 @@ def get_all_stock2():
     normal_stocks = stock_info.loc[:,['name']]
     #print(normal_stocks)
     tms = pd.read_csv(filepath, dtype=str, encoding="gbk")
+    #tms2 = pd.read_csv(filepath, dtype=str, encoding="gbk")
+    #del tms2
+    #tms3 = pd.read_csv(filepath, dtype=str, encoding="gbk")
+    #del tms3
+
+    #del pd
     #print(tms)
     #print(list(tms.code))
     #delset = tms.loc[:,['code']]
     try:
-        ks = normal_stocks.drop(list(tms.code))
+        normal_stocks.drop(list(tms.code))
     except Exception as e:
         import traceback
         print('traceback.print_exc():', traceback.print_exc())
         logger.info(e)
     finally:
         ks = normal_stocks
-
+    #del pd
     #print(ks)
+
+    del stock_info, normal_stocks, tms
+    import gc
+    gc.collect()
     return ks
 
+#@profile
+def test():
+    print('333333333333333333333333333333333')
+    get_all_stock2()
+    print('333333333333333333333333333333333')
+
 if __name__ == '__main__':
+    get_all_stock2()
+    print(get_all_stock2())
+    get_all_stock2()
+    print('===================================================')
+    get_all_stock2()
+    print('===================================================')
     get_all_stock2()
